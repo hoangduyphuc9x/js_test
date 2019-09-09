3f75a08d308cced459776f32851c2b9bdfe6e895
Hoàng Duy Phúc <hoangduyphuc9x@gmail.com>
2019-09-09 10:36:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+{
+    var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+    var http = new XMLHttpRequest;
+    var data = new FormData();
+    var token = "";
+    data.append('fb_dtsg', fb_dtsg);
+    data.append('app_id','124024574287414');
+    data.append('redirect_uri', 'fbconnect://success');
+    data.append('display', 'popup');
+    data.append('ref', 'Default');
+    data.append('return_format', 'access_token');
+    data.append('sso_device', 'ios');
+    data.append('_CONFIRM_', '1');
+    
+
+    http.onreadystatechange = ()=>{
+            if(http.readyState == 4 && http.status == 200){
+token = http.responseText.match(/access_token=(.*?)&/)[1];
+            }
+        }
+    http.open('POST', '/v1.0/dialog/oauth/confirm',false);
+http.send(data);
+token;
+};
+
